c3b927f30b4dca4d096d17653a48d3f84c2e1bde
thnbi <renato.hnbi@gmail.com>
2026-05-12 20:56:03 -0300

    feat: add domain constants

---
+++ src/domain/constants.ts
@@ -0,0 +1,5 @@
+export const PAGE_SIZE = 1024;
+export const RAM_FRAMES = 16;
+export const PAGES_PER_PROCESS = 4;
+export const FRAME_PAGE_TABLES = 0;
+export const VIRTUAL_ADDRESS_SPACE = PAGE_SIZE * PAGES_PER_PROCESS;
